a4ac7b36619477d0fb01449fe91476a9adfaa994
sanghwacode <146323503+sanghwacode@users.noreply.github.com>
2023-09-28 18:13:57 +0900

    Update main.cpp

--- 객체지향프로그램과제_1/main.cpp
+++ 객체지향프로그램과제_1/main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main() {
 	Ram ram;
-	ram.write(100, 20);
-	ram.write(101, 30);
+	ram.write(10, 100);
+	ram.write(11, 101);
 	char res = ram.read(100) + ram.read(101);
 	ram.write(102, res);
 	cout << "102번지의 " << (int)ram.read(102) << endl;
