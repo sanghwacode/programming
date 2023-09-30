8f4aed3e968c477d06fde805f534dee9aeb90219
sanghwacode <146323503+sanghwacode@users.noreply.github.com>
2023-09-30 15:16:25 +0900

    Update main.cpp

--- 객체지향프로그램과제_1/main.cpp
+++ 객체지향프로그램과제_1/main.cpp
@@ -1,14 +1,15 @@
-#include "Ram.h"
 #include <iostream>
+#include "Ram.h"
 using namespace std;
 
-int main() {
-	Ram ram;
-	ram.write(10, 100);
-	ram.write(11, 101);
-	char res = ram.read(10) + ram.read(11);
-	ram.write(12, res);
-	cout << "12번째 메모리의 값 = " << (int)ram.read(12) << endl;
 
-	return 0;
+int main()
+{
+    Ram ram;
+    ram.write(100, 20);
+    ram.write(101, 30);
+    char res = ram.read(100) + ram.read(101);
+    ram.write(102, res);
+    cout << "102 번지의 값 = " << (int)ram.read(102) << endl;
+
 }
